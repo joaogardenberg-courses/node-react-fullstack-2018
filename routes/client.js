30737e32bd3122c5a9eaab5453f71932609cb4d9
João Gardenberg <joaogardenberg@gmail.com>
2019-06-07 06:48:53 -0300

    Fix

--- routes/client.js
+++ routes/client.js
@@ -12,7 +12,7 @@ module.exports = (app) => {
     const path = require('path');
 
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
     });
   }
 };
